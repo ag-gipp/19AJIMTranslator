bd0a9f27f0a69cd639a0f9b674d5bf07eb9cf95d
AndreG-P <andre.greiner-petter@t-online.de>
2018-05-22 17:30:05 +0900

    Init commit with original master thesis

---
+++ del.sh
@@ -0,0 +1,15 @@
+rm -f Masterthesis.acn
+rm -f Masterthesis.acr
+rm -f Masterthesis.alg
+rm -f Masterthesis.aux
+rm -f Masterthesis.bbl
+rm -f Masterthesis.bcf
+rm -f Masterthesis.blg
+rm -f Masterthesis.ist
+rm -f Masterthesis.loa
+rm -f Masterthesis.lof
+rm -f Masterthesis.lot
+rm -f Masterthesis.toc
+rm -f Masterthesis.out
+rm -f Masterthesis.run.xml
+rm -f Masterthesis.synctex.gz
